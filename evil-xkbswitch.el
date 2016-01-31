1e65397c613d81686f46288221e383bac9d149c8
Quang-Linh LE <linktohack@gmail.com>
2016-01-31 17:56:27 +0100

    Silence, please

--- evil-xkbswitch.el
+++ evil-xkbswitch.el
@@ -2,30 +2,36 @@
 
 (defvar evil-xkbswitch-binary "issw"
   "Switch input binary.")
-(defvar evil-xkbswitch-us-method "com.apple.keylayout.US"
+(defvar evil-xkbswitch--us-method "com.apple.keylayout.US"
   "US input method.")
-(defvar evil-xkbswitch-last-method nil
+(defvar evil-xkbswitch--last-method nil
   "Last input method.")
+(defvar evil-xkbswitch-verbose nil
+  "Verbose?")
 
 (defun evil-xkbswitch-to-us ()
   "Switch input method to US.
-Save last method into `evil-xkbswitch-last-method'."
+Save last method into `evil-xkbswitch--last-method'."
   (interactive)
-  (setq evil-xkbswitch-last-method
+  (setq evil-xkbswitch--last-method
         (shell-command-to-string
          (format "%s" evil-xkbswitch-binary)))
-  (shell-command (format "%s %s"
-                         evil-xkbswitch-binary
-                         evil-xkbswitch-us-method)))
+  (shell-command-to-string (format "%s %s"
+                                   evil-xkbswitch-binary
+                                   evil-xkbswitch--us-method))
+  (when evil-xkbswitch-verbose
+    (message "Current method %s" evil-xkbswitch--us-method)))
 
 (defun evil-xkbswitch-to-alternate ()
   "Restore last input method."
   (interactive)
-  (when evil-xkbswitch-last-method
-    (shell-command
+  (when evil-xkbswitch--last-method
+    (shell-command-to-string
      (format "%s %s"
              evil-xkbswitch-binary
-             evil-xkbswitch-last-method))))
+             evil-xkbswitch--last-method)))
+  (when evil-xkbswitch-verbose
+    (message "Current method %s" evil-xkbswitch--last-method)))
 
 ;;;###autoload
 (define-minor-mode evil-xkbswitch-mode
