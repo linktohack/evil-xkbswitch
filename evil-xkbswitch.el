4fd08e0433ae4cfb8f1f4bef6a54393435899a11
Layerex <layerex@dismail.de>
2023-01-06 21:33:21 +0300

    Switch layout on key read (r, f, t, etc.)

--- evil-xkbswitch.el
+++ evil-xkbswitch.el
@@ -1,5 +1,7 @@
 ;;; evil-xkbswitch.el --- Input method switching corresponds to current state.
 
+(require 'evil)
+
 (defvar evil-xkbswitch-set-layout (if (eq system-type 'darwin) "issw" "xkb-switch -s")
   "Set xkb layout.")
 (defvar evil-xkbswitch-get-layout (if (eq system-type 'darwin) "issw" "xkb-switch")
@@ -35,6 +37,21 @@ Save last method into `evil-xkbswitch--last-method'."
   (when evil-xkbswitch-verbose
     (message "Current method (alternate) %s" evil-xkbswitch--last-method)))
 
+(defun evil-xkbswitch--wrapper-function (original-fun &rest args)
+  "Use alternate layout while ORIGINAL-FUN called with ARGS is running."
+  (prog2
+    (evil-xkbswitch-to-alternate)
+    (apply original-fun args)
+    (evil-xkbswitch-to-us)))
+
+(defun evil-xkbswitch--wrap-function (fun)
+  "Advice to use alternate layout while FUN is running."
+  (advice-add fun :around #'evil-xkbswitch--wrapper-function))
+
+(defun evil-xkbswitch--unwrap-function (fun)
+  "Revert advice to use alternate layout while FUN is running."
+  (advice-remove fun #'evil-xkbswitch--wrapper-function))
+
 ;;;###autoload
 (define-minor-mode evil-xkbswitch-mode
   "Switch input."
@@ -45,10 +62,12 @@ Save last method into `evil-xkbswitch--last-method'."
         (add-hook 'evil-insert-state-entry-hook #'evil-xkbswitch-to-alternate)
         (add-hook 'evil-insert-state-exit-hook #'evil-xkbswitch-to-us)
         (add-hook 'evil-replace-state-entry-hook #'evil-xkbswitch-to-alternate)
-        (add-hook 'evil-replace-state-exit-hook #'evil-xkbswitch-to-us))
+        (add-hook 'evil-replace-state-exit-hook #'evil-xkbswitch-to-us)
+        (evil-xkbswitch--wrap-function #'evil-read-key))
     (remove-hook 'evil-insert-state-entry-hook #'evil-xkbswitch-to-alternate)
     (remove-hook 'evil-insert-state-exit-hook #'evil-xkbswitch-to-us)
     (remove-hook 'evil-replace-state-entry-hook #'evil-xkbswitch-to-alternate)
-    (remove-hook 'evil-replace-state-exit-hook #'evil-xkbswitch-to-us)))
+    (remove-hook 'evil-replace-state-exit-hook #'evil-xkbswitch-to-us)
+    (evil-xkbswitch--unwrap-function #'evil-read-key)))
 
 (provide 'evil-xkbswitch)
